ccfc836c8b98d1d335ad914670f5e91f6ebc3066
salo-002 <salina.bek01@gmail.com>
2025-03-13 00:39:44 +0300

    Update test1_1.cpp

--- test1_1/test1_1.cpp
+++ test1_1/test1_1.cpp
@@ -4,19 +4,19 @@ using namespace std;
 int main() {
     int first_number, second_number;
 
-    // Запрос первого целого числа
+    Запрос первого целого числа
     cout << "Enter an integer: ";
     cin >> first_number;
 
-    // Запрос второго целого числа
+    Запрос второго целого числа
     cout << "Enter another integer: ";
     cin >> second_number;
 
-    // Вычисление суммы и разности
+    Вычисление суммы и разности
     int sum_result = first_number + second_number;
     int difference_result = first_number - second_number;
 
-    // Вывод результатов
+    Вывод результатов
     cout << first_number << " + " << second_number << " is " << sum_result << "." << endl;
     cout << first_number << " - " << second_number << " is " << difference_result << "." << endl;
 
