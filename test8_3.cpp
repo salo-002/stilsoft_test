5a4092dec3e248831c9eef08c12d39d97cae4c97
salo-002 <salina.bek01@gmail.com>
2025-03-13 01:12:33 +0300

    Create test8_3.cpp

---
+++ test8_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+Шаблон функции mult
+template <typename T>
+T mult(T a, int b)
+{
+    return a * b;
+}
+
+int main()
+{
+    std::cout << mult(2, 3) << '\n';       Выводит 6
+    std::cout << mult(1.2, 3) << '\n';     Выводит 3.6
+
+    return 0;
+}
