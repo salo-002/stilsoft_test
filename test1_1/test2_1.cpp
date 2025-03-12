a819b0db2372353a61475d5a2eafe5d903d6537f
salo-002 <salina.bek01@gmail.com>
2025-03-13 00:38:43 +0300

    Update test2_1.cpp

--- test1_1/test2_1.cpp
+++ test1_1/test2_1.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-// Function to read an integer from the user
+Function to read an integer from the user
 int readNumber() {
   int number;
   cout << "Enter an integer: ";
@@ -10,22 +10,22 @@ int readNumber() {
   return number;
 }
 
-// Function to write the answer to the console
+Function to write the answer to the console
 void writeAnswer(int answer) {
   cout << "The sum is: " << answer << endl;
 }
 
 int main() {
-  // Read the first number
+Read the first number
   int num1 = readNumber();
 
-  // Read the second number
+  Read the second number
   int num2 = readNumber();
 
-  // Calculate the sum
+  Calculate the sum
   int sum = num1 + num2;
 
-  // Write the answer
+  Write the answer
   writeAnswer(sum);
 
   return 0;
